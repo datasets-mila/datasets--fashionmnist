datasets-mila/datasets--mnist

Add torchvision preprocessing scripts

scripts/preprocess_torchvision.py
@@ -1,0 +1,5 @@
+import torchvision
+
+torchvision.datasets.MNIST(".", train=True, download=True)
+torchvision.datasets.MNIST(".", train=False, download=True)
+

datasets-mila/datasets--fashionmnist

scripts/preprocess_torchvision.py
@@ -1,0 +1,4 @@
+import torchvision
+
+torchvision.datasets.FashionMNIST(".", train=True, download=True)
+torchvision.datasets.FashionMNIST(".", train=False, download=True)